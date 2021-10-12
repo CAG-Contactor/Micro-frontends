22c2ccaa46240c4c08eba6f10d17870d4e917daf
Joel Binnquist <joel.binnquist@maxm.se>
2021-10-12 17:20:25 +0200

    Fixa backend03

--- backend03/src/main/java/se/caglabs/microfe/backend03/Backend03Application.java
+++ backend03/src/main/java/se/caglabs/microfe/backend03/Backend03Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Backend02Application {
+public class Backend03Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Backend01Application.class, args);
+		SpringApplication.run(Backend03Application.class, args);
 	}
 
 }
--- backend03/src/test/java/se/caglabs/microfe/backend03/Backend03ApplicationTests.java
+++ backend03/src/test/java/se/caglabs/microfe/backend03/Backend03ApplicationTests.java
@@ -1,10 +1,10 @@
-package se.caglabs.microfe.backend01;
+package se.caglabs.microfe.backend03;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Backend01ApplicationTests {
+class Backend03ApplicationTests {
 
 	@Test
 	void contextLoads() {
